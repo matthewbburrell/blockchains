1b1cad388ac62dbd984b892498f3256bc05fe563
MattBurr77 <you@example.com>
2020-11-01 20:21:01 -0800

    first commit

---
+++ blockchain.py
@@ -0,0 +1,24 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.cuurent_transactions = []
+
+    def new_block(self):
+        #Creates a new bloack and adds it to the chain
+        pass
+
+    def new_transaction(self):
+        # Adds a new tarnasaction to the list of trnsactions
+        pass
+
+    @staticmethod 
+    def has(block):
+        #hashes a Block
+        pass 
+
+    @property
+    def last_block(self):
+        #returns the last block in the chain
+        pass
+
+        
